ea838ef9e65b04f4000fff6d66aca7af1c15c5fa
samuzora <lucastanyj@gmail.com>
2022-01-30 17:14:15 +0800

    Added new guild to beta

--- bot/config.py
+++ bot/config.py
@@ -3,4 +3,4 @@ import os
 MYSQL_PW = os.getenv('MYSQL_ROOT_PASSWORD')
 MYSQL_DB = os.getenv('MYSQL_DB')
 prefix = None
-beta_guilds = [877477283655450654]
+beta_guilds = [877477283655450654, 801425873017503756]
