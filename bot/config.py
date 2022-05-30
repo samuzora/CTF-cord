8158080c5b60a928e2e266a51a89364ca5ac72cb
samuzora <lucastanyj@gmail.com>
2022-05-30 23:47:10 +0800

    Removed beta_guilds.txt req

--- bot/config.py
+++ bot/config.py
@@ -5,8 +5,11 @@ import mysql.connector
 MYSQL_PW = os.getenv('MYSQL_ROOT_PASSWORD')
 MYSQL_DB = os.getenv('MYSQL_DB')
 prefix = None
-beta_guilds = [a.strip() for a in open('beta_guilds.txt').readlines()]
-beta_guilds = beta_guilds if beta_guilds != [] else None
+try:
+    beta_guilds = [a.strip() for a in open('beta_guilds.txt').readlines()]
+    beta_guilds = beta_guilds if beta_guilds != [] else None
+except:
+    beta_guilds = None
 
 # --- class to connect to db ---
 class Connect():
@@ -23,18 +26,3 @@ class Connect():
     
     def __exit__(self, type, value, traceback):
         self.cnx.close()
-
-# --- internal command to get user's team ---
-async def get_user_team(user, guild):
-    with Connect() as cnx:
-        cursor = cnx.cursor()
-        # lord forgive me for sqli
-        cursor.execute(
-                'SELECT id FROM teams '\
-                f'WHERE guild = %s AND role IN ({", ".join([str(role.id) for role in user.roles])})',
-                (guild.id,),
-        )
-        try:
-            return cursor.fetchall()[0][0]
-        except IndexError:
-            return None
